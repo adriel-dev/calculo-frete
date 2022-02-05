71f83ab235f29d11a85cbb08619912aa8db1d23a
Adriel Felix <adrielandrade3@gmail.com>
2022-02-05 14:28:35 -0300

    myqsl database

---
+++ src/main/java/com/adriel/frete/config/SpringFoxConfig.java
@@ -0,0 +1,49 @@
+package com.adriel.frete.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+
+    @Bean
+    public Docket swagger() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.adriel.frete.controllers"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(info())
+                .useDefaultResponseMessages(false);
+    }
+
+    private ApiInfo info() {
+        ApiInfo apiInfo = new ApiInfo(
+                "Consulta Frete",
+                "API REST para consulta de preço de frete.",
+                "1.0",
+                "Termos de Serviço",
+                new Contact("Adriel Felix", "https://www.linkedin.com/in/adriel-felix/", "adrielandrade3@gmail.com"),
+                "Github",
+                "https://github.com/adriel-dev",
+                new ArrayList<VendorExtension>());
+        return apiInfo;
+    }
+
+}
--- src/main/java/com/adriel/frete/controllers/FreteController.java
+++ src/main/java/com/adriel/frete/controllers/FreteController.java
@@ -4,18 +4,28 @@ import com.adriel.frete.services.FreteService;
 import com.adriel.frete.util.ResponseModel;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController @RequestMapping(value = "/frete")
+@Api(value = "Consulta Frete") @CrossOrigin(origins = "*")
+@ApiResponses(value = {
+    @ApiResponse(code = 200, message = "OK"),
+    @ApiResponse(code = 400, message = "Erro!!")})
 public class FreteController {
     
     @Autowired
     FreteService freteService;
 
-    @GetMapping
+    @GetMapping @ApiOperation(value = "Retorna o valor do frete e a data prevista de entrega.")
     public ResponseModel consultaFrete(@RequestParam Float peso, @RequestParam String cepOrigem, @RequestParam String cepDestino,
     @RequestParam String nomeDestinatario)
     {
